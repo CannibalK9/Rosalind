2c306e10305647b92b3a8b792a21456185b6f48d
Dan <ddaburns@gmail.com>
2017-11-13 10:58:36 +0000

    please merge properly

---
+++ Rosalind/Rosalind/Tier7/FindingASharedSplicedMotifStolen.cs
@@ -0,0 +1,116 @@
+﻿using Rosalind.Converters;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Rosalind.Tier7
+{
+    public class FindingASharedSplicedMotifStolen
+    {
+        //http://rosalind.info/problems/lcsq/
+
+        public FindingASharedSplicedMotifStolen()
+        {
+            List<string> dnaStrings = FASTAToDictionary.Convert(File.ReadAllLines(@"C:\code\dataset.txt").ToList()).Values.ToList();
+            var pairs = new Dictionary<KeyValuePair<int, int>, int>();
+            dod(dnaStrings[0], dnaStrings[1], dnaStrings[0].Length - 1, dnaStrings[1].Length - 1, pairs);
+            string sequence = reconstruct(dnaStrings[0], dnaStrings[1], dnaStrings[0].Length - 1, dnaStrings[1].Length - 1, pairs);
+            sequence = string.Join("", sequence.Reverse());
+            Console.WriteLine(sequence.Length);
+            Console.WriteLine(sequence);
+            //Console.WriteLine(Environment.NewLine);
+            //Console.WriteLine(bluh(dnaStrings[0], dnaStrings[1], dnaStrings[0].Length - 1, dnaStrings[1].Length - 1));
+        }
+
+        private string bluh(string s1, string s2, int s1Length, int s2Length)
+        {
+            if (s1Length < 0 || s2Length < 0)
+            {
+                return "";
+            }
+            else if (s1[s1Length] == s2[s2Length])
+            {
+                return bluh(s1, s2, s1Length - 1, s2Length - 1) + s1[s1Length];
+            }
+            else
+            {
+                string new1 = bluh(s1, s2, s1Length, s2Length - 1);
+                string new2 = bluh(s1, s2, s1Length - 1, s2Length);
+
+                return new1.Length > new2.Length ? new1 : new2;
+            }
+        }
+
+        private int dod(string s1, string s2, int s1Length, int s2Length, Dictionary<KeyValuePair<int, int>, int> pairs)
+        {
+            var kvp = new KeyValuePair<int, int>(s1Length, s2Length);
+
+            if (s1Length < 0 || s2Length < 0)
+            {
+                return 0;
+            }
+            else if (pairs.ContainsKey(kvp))
+            {
+                return pairs[kvp];
+            }
+            else
+            {
+                int one = dod(s1, s2, s1Length - 1, s2Length, pairs);
+                int two = dod(s1, s2, s1Length, s2Length - 1, pairs);
+                int three = dod(s1, s2, s1Length - 1, s2Length - 1, pairs);
+
+                if (s1[s1Length] == s2[s2Length])
+                    three++;
+
+                pairs[kvp] = Math.Max(one, Math.Max(two, three));
+                return pairs[kvp];
+            }
+        }
+        private string reconstruct(string s1, string s2, int s1Length, int s2Length, Dictionary<KeyValuePair<int, int>, int> pairs)
+        {
+            var sequence = new List<char>();
+
+            while (s1Length >= 0 && s2Length >= 0)
+            {
+                int diag = 0;
+                int left = 0;
+                int up = 0;
+
+                pairs.TryGetValue(new KeyValuePair<int, int>(s1Length - 1, s2Length - 1), out diag);
+                pairs.TryGetValue(new KeyValuePair<int, int>(s1Length, s2Length - 1), out left);
+                pairs.TryGetValue(new KeyValuePair<int, int>(s1Length - 1, s2Length), out up);
+
+                if (s1[s1Length] == s2[s2Length])
+                    diag++;
+
+                var options = new List<int> { diag, left, up };
+                int curr = 0;
+
+                for (int i = 0; i < options.Count; i++)
+                {
+                    if (options[i] > options[curr])
+                        curr = i;
+                }
+
+                if (curr == 0)
+                {
+                    if (s1[s1Length] == s2[s2Length])
+                        sequence.Add(s1[s1Length]);
+                    s1Length--;
+                    s2Length--;
+                }
+                else if (curr == 1)
+                {
+                    s2Length--;
+                }
+                else
+                {
+                    s1Length--;
+                }
+            }
+
+            return string.Join("", sequence);
+        }
+    }
+}
--- Rosalind/Rosalind/Tier9/EditDistanceAlignment.cs
+++ Rosalind/Rosalind/Tier9/EditDistanceAlignment.cs
@@ -14,26 +14,71 @@ namespace Rosalind.Tier9
         {
             List<string> dnaStrings = FASTAToDictionary.Convert(File.ReadAllLines(@"C:\code\dataset.txt").ToList()).Values.ToList();
 
-            var s1Hyphens = new List<int>();
-            var s2Hyphens = new List<int>();
+            var pairs = new Dictionary<KeyValuePair<int, int>, KeyValuePair<int, int>>();
 
-            int changeCount = Distance(dnaStrings[0], dnaStrings[1], dnaStrings[0].Length - 1, dnaStrings[1].Length - 1,s1Hyphens, s2Hyphens, new Dictionary<KeyValuePair<int, int>, int>());
+            int changeCount = Distance(dnaStrings[0], dnaStrings[1], dnaStrings[0].Length - 1, dnaStrings[1].Length - 1, pairs);
 
             var s1Chars = new List<char>(dnaStrings[0]);
             var s2Chars = new List<char>(dnaStrings[1]);
 
-            foreach (int hyphenIndex in s1Hyphens)
-                s1Chars.Insert(hyphenIndex, '-');
+            int i = pairs.Last().Key.Key;
+            int j = pairs.Last().Key.Value;
+            KeyValuePair<int, int> curr = new KeyValuePair<int, int>(i, j);
 
-            foreach (int hyphenIndex in s2Hyphens)
-                s2Chars.Insert(hyphenIndex, '-');
+            AddHyphens(pairs, curr, s1Chars, s2Chars);
+
+            while (s1Chars.Count < s2Chars.Count)
+            {
+                s1Chars.Insert(0, '-');
+            }
+
+            while (s1Chars.Count > s2Chars.Count)
+            {
+                s2Chars.Insert(0, '-');
+            }
 
             Console.WriteLine(changeCount);
             Console.WriteLine(string.Join("", s1Chars));
             Console.WriteLine(string.Join("", s2Chars));
         }
 
-        private int Distance(string s1, string s2, int s1Length, int s2Length, List<int>s1Hyphens, List<int>s2Hyphens, Dictionary<KeyValuePair<int,int>,int> pairs)
+        private void AddHyphens(Dictionary<KeyValuePair<int, int>, KeyValuePair<int, int>> pairs, KeyValuePair<int, int> pair, List<char> s1Chars, List<char> s2Chars)
+        {
+            var diag = new KeyValuePair<int, int>(pair.Key - 1, pair.Value - 1);
+            var up = new KeyValuePair<int, int>(pair.Key, pair.Value - 1);
+            var left = new KeyValuePair<int, int>(pair.Key - 1, pair.Value);
+
+            KeyValuePair<int, int> diagValue = pairs.ContainsKey(diag) ? pairs[diag] : new KeyValuePair<int, int>(-1, -1);
+            KeyValuePair<int, int> upValue = pairs.ContainsKey(up) ? pairs[up] : new KeyValuePair<int, int>(-1, -1);
+            KeyValuePair<int, int> leftValue = pairs.ContainsKey(left) ? pairs[left] : new KeyValuePair<int, int>(-1, -1);
+
+            if (diagValue.Key >= 0 && diagValue.Key <= upValue.Key && diagValue.Key <= leftValue.Key)
+            {
+                if (diagValue.Value == 1)
+                    s1Chars.Insert(pair.Key, '-');
+                else if (diagValue.Value == 2)
+                    s2Chars.Insert(pair.Value, '-');
+                AddHyphens(pairs, diag, s1Chars, s2Chars);
+            }
+            else if (upValue.Key >= 0 && upValue.Key <= leftValue.Key)
+            {
+                if (upValue.Value == 1)
+                    s1Chars.Insert(pair.Key, '-');
+                else if (upValue.Value == 2)
+                    s2Chars.Insert(pair.Value, '-');
+                AddHyphens(pairs, up, s1Chars, s2Chars);
+            }
+            else if (leftValue.Key >= 0)
+            {
+                if (leftValue.Value == 1)
+                    s1Chars.Insert(pair.Key, '-');
+                else if (leftValue.Value == 2)
+                    s2Chars.Insert(pair.Value, '-');
+                AddHyphens(pairs, left, s1Chars, s2Chars);
+            }
+        }
+
+        private int Distance(string s1, string s2, int s1Length, int s2Length, Dictionary<KeyValuePair<int, int>, KeyValuePair<int, int>> pairs)
         {
             var pair = new KeyValuePair<int, int>(s1Length, s2Length);
 
@@ -44,34 +89,26 @@ namespace Rosalind.Tier9
             else if (s1[s1Length] == s2[s2Length])
             {
                 if (pairs.ContainsKey(pair))
-                    return pairs[pair];
+                    return pairs[pair].Key;
                 else
                 {
-                    int d = Distance(s1, s2, s1Length - 1, s2Length - 1, s1Hyphens, s2Hyphens, pairs);
-                    pairs.Add(pair, d);
+                    int d = Distance(s1, s2, s1Length - 1, s2Length - 1, pairs);
+                    pairs.Add(pair, new KeyValuePair<int, int>(d, 0));
                     return d;
                 }
             }
             else
             {
                 if (pairs.ContainsKey(pair))
-                    return pairs[pair];
+                    return pairs[pair].Key;
                 else
                 {
-                    int s1Count = 1 + Distance(s1, s2, s1Length - 1, s2Length, s1Hyphens, s2Hyphens, pairs);
-                    int s2Count = 1 + Distance(s1, s2, s1Length, s2Length - 1, s1Hyphens, s2Hyphens, pairs);
-                    int changeCount = 1 + Distance(s1, s2, s1Length - 1, s2Length - 1, s1Hyphens, s2Hyphens, pairs);
-
-                    if (s1Count < changeCount && s2Count < changeCount)
-                    {
-                        if (s1Count < s2Count)
-                            s1Hyphens.Add(s1Length - 1);
-                        else
-                            s2Hyphens.Add(s2Length - 1);
-                    }
+                    int s1Count = 1 + Distance(s1, s2, s1Length - 1, s2Length, pairs);
+                    int s2Count = 1 + Distance(s1, s2, s1Length, s2Length - 1, pairs);
+                    int changeCount = 1 + Distance(s1, s2, s1Length - 1, s2Length - 1, pairs);
 
                     int d = Math.Min(s1Count, Math.Min(s2Count, changeCount));
-                    pairs.Add(pair, d);
+                    pairs.Add(pair, new KeyValuePair<int, int>(d, changeCount <= s1Count && changeCount <= s2Count ? 0 : s1Count < s2Count ? 2 : 1));
                     return d;
                 }
             }
