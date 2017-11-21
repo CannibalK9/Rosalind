7071e39aeb3aabbfcbfd251dfa0a2b91ea0fd3fe
DB <ddaburns@gmail.com>
2017-11-21 08:00:52 +0000

    actually worthless

--- Rosalind/Rosalind/Tier6/PerfectMatchingAndRnaSecondaryStructures.cs
+++ Rosalind/Rosalind/Tier6/PerfectMatchingAndRnaSecondaryStructures.cs
@@ -15,52 +15,50 @@ namespace Rosalind.Tier6
             string input = FASTAToDictionary.Convert(File.ReadAllLines(@"C:\code\dataset.txt").ToList()).First().Value;
             _perfectLength = input.Length / 2;
 
-            var result = new List<List<int>>();
-            Matches(input, result, new List<int>());
+            var result = new List<List<KeyValuePair<int, int>>>();
+            Matches(input, result, new List<KeyValuePair<int, int>>());
+            Console.WriteLine(result.Count);
         }
 
         private Dictionary<char, char> _validConnections = new Dictionary<char, char> { { 'U', 'A' }, { 'A', 'U' }, { 'C', 'G' }, { 'G', 'C' } };
         private int _perfectLength;
 
-        private void Matches(string input, List<List<int>> matchesCollection, List<int> matches)
+        private void Matches(string input, List<List<KeyValuePair<int, int>>> matchesCollection, List<KeyValuePair<int, int>> currentMatch)
         {
-            for (int i = 0; i < input.Length; i++)
+            int i = 0;
+            if (currentMatch.Any())
             {
-                for (int j = 0; j < input.Length; j++)
+                for (int k = 0; k < input.Length; k++)
                 {
-                    if (Math.Abs(i - j) <= 1 || _validConnections[input[i]] != input[j] || matches.Any(m => m / input.Length == i || m % input.Length == j || m / input.Length == j || m % input.Length == i))
-                        continue;
-
-                    var newMatches = new List<int>(matches);
-                    if (i < j)
-                        newMatches.Add(i * input.Length + j);
-                    else
-                        newMatches.Add(j * input.Length + i);
-
-                    if (newMatches.Count == _perfectLength)
+                    if (currentMatch.All(c => c.Key != k && c.Value != k))
                     {
-                        newMatches.Sort();
-
-                        if (matchesCollection.Any())
-                        {
-                            foreach (var match in matchesCollection)
-                            {
-                                bool same = true;
-                                for (int k = 0; k < _perfectLength; k++)
-                                {
-                                    if (match[k] != newMatches[k])
-                                        same = false;
-                                }
-
-                                if (same)
-                                    return;
-                            }
-                        }
-                        matchesCollection.Add(newMatches);
-                        return;
+                        i = k;
+                        break;
                     }
-                    Matches(input, matchesCollection, newMatches);
                 }
+                if (i == 0)
+                    return;
+            }
+
+            var buildingMatches = new List<List<KeyValuePair<int, int>>>();
+
+            for (int j = i + 1; j < input.Length; j++)
+            {
+                if (_validConnections[input[i]] != input[j] || currentMatch.Any(c => c.Key == j || c.Value == j))
+                    continue;
+
+                var newMatches = new List<KeyValuePair<int, int>>(currentMatch);
+                newMatches.Add(new KeyValuePair<int, int>(i, j));
+
+                buildingMatches.Add(newMatches);
+            }
+
+            foreach (var item in buildingMatches)
+            {
+                if (item.Count == _perfectLength)
+                    matchesCollection.Add(item);
+                else
+                    Matches(input, matchesCollection, item);
             }
         }
     }
