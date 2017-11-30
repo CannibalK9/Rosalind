ed5ba81f68f4a41fa6315fb99c6af8a03b56bf5f
DB <ddaburns@gmail.com>
2017-11-30 08:06:43 +0000

    Some broken tree thing

--- Rosalind/Rosalind/Tier9/FindingTheLongestMultipleRepeat.cs
+++ Rosalind/Rosalind/Tier9/FindingTheLongestMultipleRepeat.cs
@@ -11,41 +11,55 @@ namespace Rosalind.Tier9
 
         public FindingTheLongestMultipleRepeat()
         {
-            var trie = new Dictionary<KeyValuePair<int, int>, char>();
+            List<string> inputs = File.ReadAllLines(@"C:\code\dataset.txt").ToList();
+            var lengthFromNodes = new Dictionary<int, int>();
+            var edges = new List<Edge>();
+            string dna = inputs[0];
+            int repeats = Convert.ToInt32(inputs[1]);
 
-            IEnumerable<string> inputs = File.ReadAllLines(@"C:\code\dataset.txt");
-
-            foreach (string dna in inputs)
+            for (int i = 2; i < inputs.Count; i++)
             {
-                int nextNode = 1;
-                bool newChain = false;
-                foreach (char c in dna)
-                {
-                    if (newChain == false)
-                    {
-                        if (trie.Any(t => t.Value == c && t.Key.Key == nextNode))
-                        {
-                            nextNode = trie.Single(t => t.Value == c && t.Key.Key == nextNode).Key.Value;
-                        }
-                        else
-                            newChain = true;
-                    }
-
-                    if (newChain)
-                    {
-                        int lastNode = trie.Count + 2;
-                        trie.Add(new KeyValuePair<int, int>(nextNode, lastNode), c);
-                        nextNode = lastNode;
-                    }
-                }
+                edges.Add(new Edge(inputs[i].Replace("node", "").Split(' ').Select(n=>Convert.ToInt32(n)).ToArray()));
             }
 
-            foreach (var item in trie)
+            List<int> repeatedSubstrings = edges.GroupBy(e => e.Parent).Where(g => g.Count() >= repeats).Select(g=>g.First().Parent).Where(i=> i != 1).ToList();
+
+            foreach (int childNode in repeatedSubstrings)
             {
-                Console.WriteLine(string.Format("{0} {1} {2}",
-                    item.Key.Key,
-                    item.Key.Value,
-                    item.Value));
+                int parentNode = childNode;
+                int length = 0;
+                int location = 0;
+
+                while (parentNode != 1)
+                {
+                    Edge thisEdge = edges.Single(e => e.Child == parentNode);
+                    parentNode = thisEdge.Parent;
+                    length += thisEdge.Length;
+                    if (location == 0)
+                        location = thisEdge.Location + length;
+                }
+
+                if (lengthFromNodes.ContainsKey(length) == false)
+                    lengthFromNodes.Add(length, location);
+            }
+
+            var kvp = lengthFromNodes.OrderByDescending(l => l.Key).First();
+            Console.WriteLine(dna.Substring(kvp.Value - kvp.Key - 1, kvp.Key));
+        }
+
+        private class Edge
+        {
+            public int Parent;
+            public int Child;
+            public int Location;
+            public int Length;
+
+            public Edge(int[] nums)
+            {
+                Parent = nums[0];
+                Child = nums[1];
+                Location = nums[2];
+                Length = nums[3];
             }
         }
     }
