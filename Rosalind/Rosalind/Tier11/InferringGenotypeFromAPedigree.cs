68fc036abadc2933034e01c40fc5125dba413f3c
Dan <ddaburns@gmail.com>
2017-12-12 16:32:42 +0000

    Close, then further away, then not as close as before~?

--- Rosalind/Rosalind/Tier11/InferringGenotypeFromAPedigree.cs
+++ Rosalind/Rosalind/Tier11/InferringGenotypeFromAPedigree.cs
@@ -3,8 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Rosalind.Tier11
 {
@@ -23,65 +21,82 @@ namespace Rosalind.Tier11
                 if (string.IsNullOrEmpty(parent.Name))
                     parent.Name = node.Name;
                 else
-                    parent.Name = Combined(parent.Name, node.Name);
+                    parent.Name = NewCombined(parent.Name, node.Name);
             }
 
-            List<string> stuff = tree.Nodes[0].Name.Split(',').ToList();
-            float count = stuff.Count;
-            float aa = stuff.Count(s => s.Equals("aa"));
-            float Aa = stuff.Count(s => s.Equals("Aa"));
-            float AA = stuff.Count(s => s.Equals("AA"));
+            List<double> stuff = tree.Nodes[0].Name.Split(' ').Select(s=> Convert.ToDouble(s)).ToList();
+            double aa = stuff[2];
+            double Aa = stuff[1];
+            double AA = stuff[0];
+            double count = aa + Aa + AA;
 
-            Console.Write(aa / count + " " + Aa / count + " " + AA / count);
+            Console.Write(AA / count + " " + Aa / count + " " + aa / count);
         }
 
-        private string Combined(string s1, string s2)
+        private string NewCombined(string s1, string s2)
         {
-            List<string> list1 = s1.Split(',').ToList();
-            List<string> list2 = s2.Split(',').ToList();
+            long[] int1 = s1.ToLower().Contains("a") ? nos(s1) : s1.Split(' ').Select(n => Convert.ToInt64(n)).ToArray();
+            long[] int2 = s2.ToLower().Contains("a") ? nos(s2) : s2.Split(' ').Select(n => Convert.ToInt64(n)).ToArray();
 
-            var result = new List<string>();
+            long AA = int1[0] * int2[0] * 4 + int1[0] * int2[1] * 2 + int1[1] * int2[0] * 2 + int1[1] * int2[1];
+            long Aa = int1[0] * int2[1] * 2 + int1[1] * int2[0] * 2 + int1[1] * int2[1] * 2 + int1[2] * int2[1] * 2 + int1[1] * int2[2] * 2 + int1[0] * int2[2] * 4;
+            long aa = int1[2] * int2[2] * 4 + int1[2] * int2[1] * 2 + int1[1] * int2[2] * 2 + int1[1] * int2[1];
 
-            for (int i = 0; i < list1.Count; i++)
-            {
-                if (string.IsNullOrEmpty(list1[i]))
-                    continue;
-
-                for (int j = 0; j < list2.Count; j++)
-                {
-                    if (string.IsNullOrEmpty(list2[j]))
-                        continue;
-
-                    result.AddRange(SimpleCombine(list1[i], list2[j]));
-                }
-            }
-
-            return string.Join(",", result);
+            return AA + " " + Aa + " " + aa;
         }
 
-        private List<string> SimpleCombine(string s1, string s2)
+        private long[] nos(string str)
         {
-            var result = new List<string>();
-            int As = (s1 + s2).ToList().Count(c => c == 'A');
+            List<string> list = str.Split(',').ToList();
 
-            switch (As)
-            {
-                case 0:
-                    return new List<string> { "aa" };
-                case 1:
-                    return new List<string> { "Aa", "aa", "aa", "aa" };
-                case 2:
-                    if (s1[0] == s1[1])
-                        return new List<string> { "Aa" };
-                    else
-                        return new List<string> { "AA", "Aa", "Aa", "aa" };
-                case 3:
-                    return new List<string> { "AA", "AA", "AA", "Aa" };
-                case 4:
-                    return new List<string> { "AA" };
-                default:
-                    return new List<string>();
-            }
+            long aa = list.Count(s => s.Equals("aa"));
+            long Aa = list.Count(s => s.Equals("Aa"));
+            long AA = list.Count(s => s.Equals("AA"));
+
+            return new long[] { AA, Aa, aa };
         }
+
+        //private string Combined(string s1, string s2)
+        //{
+        //    List<string> list1 = s1.Split(',').ToList();
+        //    List<string> list2 = s2.Split(',').ToList();
+
+        //    var result = new List<string>();
+
+        //    for (long i = 0; i < list1.Count; i++)
+        //    {
+        //        for (long j = 0; j < list2.Count; j++)
+        //        {
+        //            result.AddRange(SimpleCombine(list1[i], list2[j]));
+        //        }
+        //    }
+
+        //    return string.Join(",", result);
+        //}
+
+        //private List<string> SimpleCombine(string s1, string s2)
+        //{
+        //    var result = new List<string>();
+        //    long As = (s1 + s2).ToList().Count(c => c == 'A');
+
+        //    switch (As)
+        //    {
+        //        case 0:
+        //            return new List<string> { "aa", "aa", "aa", "aa" };
+        //        case 1:
+        //            return new List<string> { "Aa", "Aa", "aa", "aa" };
+        //        case 2:
+        //            if (s1[0] == s1[1])
+        //                return new List<string> { "Aa", "Aa", "Aa", "Aa" };
+        //            else
+        //                return new List<string> { "AA", "Aa", "Aa", "aa" };
+        //        case 3:
+        //            return new List<string> { "AA", "AA", "Aa", "Aa" };
+        //        case 4:
+        //            return new List<string> { "AA", "AA", "AA", "AA" };
+        //        default:
+        //            return new List<string>();
+        //    }
+        //}
     }
 }
